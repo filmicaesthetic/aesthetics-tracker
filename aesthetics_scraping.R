c190e629d2984a16414161507a01c8882112eff7
Dan <daniel_fellowes@hotmail.co.uk>
2022-12-13 22:12:04 +0000

    update sleep time between requests

--- aesthetics_scraping.R
+++ aesthetics_scraping.R
@@ -41,13 +41,14 @@ aes_df <- aes_df |>
   filter(aesthetic != "")
 
 
-# function to add negative keywords to avoid including other aesthetics
+# function to return negative keyword sring to avoid including other aesthetics
 minus_words <- function(x) {
   
+  # split input into individual words
   words <- str_split(x, " ")[[1]]
-  
+  # set up blank list
   aes_sel <- c()
-  
+  # for each word, find matches in aesthetics list and add a negative keyword for each additional word used
   for (i in 1:length(words)) {
     
     aes_sel_it <- aes_df$aesthetic[grepl(tolower(words[i]), tolower(aes_df$aesthetic))]
@@ -67,12 +68,12 @@ minus_words <- function(x) {
     aes_sel <- c(aes_sel, aes_sel_split)
     
   }
-  
+  # remove input words and duplicates
   aes_sel <- aes_sel[!(aes_sel %in% words)]
   aes_sel <- unique(aes_sel)
-  
+  # paste into string for search
   list_x <- do.call(paste, c(as.list(aes_sel), sep = "%20-"))
-  
+  # add - to start
   list_fin <- paste0("-", list_x)
   
   return(list_fin)
@@ -82,16 +83,17 @@ minus_words <- function(x) {
 # function to look up search results on depop
 depop_results <- function(aurl) {
   
+  # get negative keyword string
   neg_key <- minus_words(aurl)
-  
+  # create url string
   url_it <- paste0("https://www.depop.com/search/?q=","%27",gsub(" ", "%20", aurl),"%27%20aesthetic%20",neg_key)
-  
+  # get search results value
   res <- url_it |>
     read_html() |>
     html_element(".Container-sc-__sc-1t5af73-0.Searchstyles__StyledContainer-sc-__sc-1ep2n60-0.ujpvx.PrZuM") |>
     html_node("b") |>
     html_text()
-  
+  # convert to numeric
   res <- as.numeric(res)
   
   return(res)
@@ -109,7 +111,7 @@ for (i in 1:nrow(aes_df)) {
   
   res <- rbind(res, res_it)
   
-  Sys.sleep(0.3)
+  Sys.sleep(0.4)
   
 }
 
